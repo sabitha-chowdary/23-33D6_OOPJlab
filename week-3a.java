1aa2ca6594c0e2a9b96511f7de60971fb8f37961
sabitha-chowdary <234G1A33D6@srit.ac.in>
2025-03-02 11:52:18 +0530

    Add files via upload

---
+++ week-3a.java
@@ -0,0 +1,26 @@
+ class demo{
+ demo(int a){
+ int f=a*a;
+  System.out.println(a);
+  }
+    demo(int a,int b){
+ int c=a*b;
+   System.out.println(c);
+  }
+   demo(int a,int b,int c){
+  int d=a*b*c;
+   System.out.println(d);
+  }
+  public static void main(String[]args){
+   demo e1 =new demo(5);
+    demo e2 =new demo(5,8);
+	 demo e3 =new demo(5,9,2);
+  }
+ }
+ 
+ 
+   
+  
+  
+    
+  
